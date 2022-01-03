f84c32c7f81b151f67db18a543adc375d8c8bb67
nearsolt <67660206+nearsolt@users.noreply.github.com>
2022-01-04 02:47:16 +0400

    Закодил 1 задание с сортировками, можно добавить многопоточность

--- ModernComputerTechnologies/ModernComputerTechnologies.cs
+++ ModernComputerTechnologies/ModernComputerTechnologies.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,27 @@ namespace ModernComputerTechnologies {
 
         #region task1: 
 
-
         static void Main(string[] args) {
+
+            int arrayLength = 100000;
+
+            int[] initialArray = new int[arrayLength];
+
+            Task1.FillRandom(initialArray, arrayLength);
+            Console.WriteLine(string.Format("Array lenght = {0}{1}", arrayLength, Environment.NewLine));
+            //Console.WriteLine(string.Format("Unsorted array:{0}{1}{0}", Environment.NewLine, string.Join(" ,", initialArray.ToArray())));
+
+            Task1.SortResult((int[])initialArray.Clone(), Task1.SortType.BubbleSort);
+            Task1.SortResult((int[])initialArray.Clone(), Task1.SortType.InsertSort);
+            Task1.SortResult((int[])initialArray.Clone(), Task1.SortType.QuickSort);
+            
+            
+            /*Stopwatch tmp = new Stopwatch();
+            int[] tmp2 = (int[])initialArray.Clone();
+            tmp.Start();
+            Array.Sort(tmp2);
+            tmp.Stop();
+            tmp2[0] = 0;*/
 
             Console.ReadKey();
         }
--- ModernComputerTechnologies/source/9sem/Task1.cs
+++ ModernComputerTechnologies/source/9sem/Task1.cs
@@ -1,10 +1,169 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ModernComputerTechnologies {
     class Task1 {
+
+        #region Helpers
+
+        /// <summary>
+        /// Виды сортировок
+        /// </summary>
+        internal enum SortType {
+            BubbleSort = 0,
+            InsertSort = 1,
+            QuickSort = 2
+        }
+
+        /// <summary>
+        /// Рандомное заполение массива, диапазон от 0 до arrayLength * 25
+        /// </summary>
+        /// <param name="array">массив</param>
+        /// <param name="arrayLength">длина массива</param>
+        internal static void FillRandom(int[] array, int arrayLength) {
+            Random randNum = new Random();
+            int maxValue = arrayLength * 25;
+            for (int i = 0; i < arrayLength; i++) {
+                array[i] = randNum.Next(0, maxValue);
+            }
+        }
+
+        /// <summary>
+        /// Результаты выполнения сортировок
+        /// </summary>
+        /// <param name="array">массив</param>
+        /// <param name="sortType">вид сортировки</param>
+        internal static void SortResult(int[] array, SortType sortType) {
+            Stopwatch stopWatch = new Stopwatch();
+            switch (sortType) {
+                case SortType.BubbleSort:
+                    stopWatch.Start();
+                    BubbleSort(array);
+                    stopWatch.Stop();
+                    break;
+                case SortType.InsertSort:
+                    stopWatch.Start();
+                    InsertSort(array);
+                    stopWatch.Stop();
+                    break;
+                case SortType.QuickSort:
+                    stopWatch.Start();
+                    QuickSort(array);
+                    stopWatch.Stop();
+                    break;
+                default:
+                    return;
+            }
+            Console.WriteLine(string.Format("Sorted array ({0}): time = {1}", sortType, stopWatch.Elapsed));
+            //Console.WriteLine(string.Format(string.Join(" ,", array.ToArray())));
+        }
+
+        #endregion
+
+        #region BubbleSort, InsertSort, QuickSort
+
+        /// <summary>
+        /// Сортировка пузырьком
+        /// </summary>
+        /// <param name="array">массив</param>
+        static void BubbleSort(int[] array) {
+            int arrayLength = array.Length;
+            for (int i = 0; i < arrayLength - 1; i++) {
+                for (int j = 0; j < arrayLength - i - 1; j++) {
+                    if (array[j] > array[j + 1]) {
+                        (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Сортировка вставками
+        /// </summary>
+        /// <param name="array">массив</param>
+        static void InsertSort(int[] array) {
+            int arrayLength = array.Length;
+            for (int i = 1; i < arrayLength; i++) {
+                for (int j = i; j > 0 && array[j - 1] > array[j]; j--) {
+                    (array[j - 1], array[j]) = (array[j], array[j - 1]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Быстрая сортировка
+        /// </summary>
+        /// <param name="array">массив</param>
+        static void QuickSort(int[] array) {
+            QuickSort(array, 0, array.Length - 1);
+        }
+
+        /// <summary>
+        /// Быстрая сортировка
+        /// </summary>
+        /// <param name="array">массив</param>
+        /// <param name="indexLow">первый индекс</param>
+        /// <param name="indexHigh">последний индекс</param>
+        static void QuickSort(int[] array, int indexLow, int indexHigh) {
+            int pivotIndex = FindPivot(array, indexLow, indexHigh);
+            if (pivotIndex != -1) {
+                int pivot = array[pivotIndex];
+                int k = Partition(array, indexLow, indexHigh, pivot);
+                QuickSort(array, indexLow, k - 1);
+                QuickSort(array, k, indexHigh);
+            }
+        }
+
+        /// <summary>
+        /// Поиск опорного элемента
+        /// </summary>
+        /// <param name="array">массив</param>
+        /// <param name="indexLow">первый индекс</param>
+        /// <param name="indexHigh">последний индекс</param>
+        /// <returns></returns>
+        static int FindPivot(int[] array, int indexLow, int indexHigh) {
+            int firstKey = array[indexLow];
+            for (int k = indexLow + 1; k <= indexHigh; k++) {
+                if (array[k] > firstKey) {
+                    return k;
+                }
+                if (array[k] < firstKey) {
+                    return indexLow;
+                }
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Разбиение массива на две части по опорному элементу
+        /// </summary>
+        /// <param name="array">массив</param>
+        /// <param name="indexLow">первый индекс</param>
+        /// <param name="indexHigh">последний индекс</param>
+        /// <param name="pivot">опорный элемент</param>
+        /// <returns></returns>
+        static int Partition(int[] array, int indexLow, int indexHigh, int pivot) {
+            int left = indexLow;
+            int right = indexHigh;
+            do {
+                while (array[left] < pivot) {
+                    left++;
+                }
+                while (array[right] >= pivot) {
+                    right--;
+                }                 
+                if (left < right) {
+                    (array[left], array[right]) = (array[right], array[left]);
+                }
+            } while (left <= right);
+            return left;
+        }
+
+
+        #endregion
     }
 }
