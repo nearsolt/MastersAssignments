eeadbe12c393e82e010218f4d3cbe62bcc31ed1a
nearsolt <67660206+nearsolt@users.noreply.github.com>
2022-01-08 10:21:06 +0400

    Закодил многопоточность для 1 таски

--- ModernComputerTechnologies/source/9sem/Task1.cs
+++ ModernComputerTechnologies/source/9sem/Task1.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ModernComputerTechnologies {
     class Task1 {
@@ -11,13 +12,44 @@ namespace ModernComputerTechnologies {
         #region Helpers
 
         /// <summary>
-        /// Виды сортировок
+        /// Функция потока для сортировки пузырьком
         /// </summary>
-        internal enum SortType {
-            BubbleSort = 0,
-            InsertSort = 1,
-            QuickSort = 2
+        /// <param name="array">массив</param>
+        internal static void ThreadBubbleSort(object array) {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Task1.BubbleSort((int[])array);
+            stopWatch.Stop();
+            //Console.WriteLine($"Sorted array <{BubbleSort}>: {string.Join(" ,", array.ToArray())}");
+            Console.WriteLine($"Sorted array <BubbleSort>: time = {stopWatch.Elapsed.Minutes}m:{stopWatch.Elapsed.Seconds}s:{stopWatch.Elapsed.Milliseconds}ms\t default time = {stopWatch.Elapsed}");
         }
+
+        /// <summary>
+        /// Функция потока для сортировки вставками
+        /// </summary>
+        /// <param name="array">массив</param>
+        internal static void ThreadInsertSort(object array) {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Task1.InsertSort((int[])array);
+            stopWatch.Stop();
+            //Console.WriteLine($"Sorted array <{InsertSort}>: {string.Join(" ,", array.ToArray())}");
+            Console.WriteLine($"Sorted array <InsertSort>: time = {stopWatch.Elapsed.Minutes}m:{stopWatch.Elapsed.Seconds}s:{stopWatch.Elapsed.Milliseconds}ms\t default time = {stopWatch.Elapsed}");
+        }
+
+        /// <summary>
+        /// Функция потока для быстрой сортировки
+        /// </summary>
+        /// <param name="array">массив</param>
+        internal static void ThreadQuickSort(object array) {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Task1.QuickSort((int[])array);
+            stopWatch.Stop();
+            //Console.WriteLine($"Sorted array <{QuickSort}>: {string.Join(" ,", array.ToArray())}");
+            Console.WriteLine($"Sorted array <QuickSort>: time = {stopWatch.Elapsed.Minutes}m:{stopWatch.Elapsed.Seconds}s:{stopWatch.Elapsed.Milliseconds}ms\t default time = {stopWatch.Elapsed}");
+        }
+
 
         /// <summary>
         /// Рандомное заполение массива, диапазон от 0 до arrayLength * 25
@@ -32,36 +64,6 @@ namespace ModernComputerTechnologies {
             }
         }
 
-        /// <summary>
-        /// Результаты выполнения сортировок
-        /// </summary>
-        /// <param name="array">массив</param>
-        /// <param name="sortType">вид сортировки</param>
-        internal static void SortResult(int[] array, SortType sortType) {
-            Stopwatch stopWatch = new Stopwatch();
-            switch (sortType) {
-                case SortType.BubbleSort:
-                    stopWatch.Start();
-                    BubbleSort(array);
-                    stopWatch.Stop();
-                    break;
-                case SortType.InsertSort:
-                    stopWatch.Start();
-                    InsertSort(array);
-                    stopWatch.Stop();
-                    break;
-                case SortType.QuickSort:
-                    stopWatch.Start();
-                    QuickSort(array);
-                    stopWatch.Stop();
-                    break;
-                default:
-                    return;
-            }
-            Console.WriteLine($"Sorted array <{sortType}>: time = {stopWatch.Elapsed.Minutes}m:{stopWatch.Elapsed.Seconds}s:{stopWatch.Elapsed.Milliseconds}ms\t default time = {stopWatch.Elapsed}");
-            //Console.WriteLine($"Sorted array <{sortType}>: {string.Join(" ,", array.ToArray())}");
-        }
-
         #endregion
 
         #region BubbleSort, InsertSort, QuickSort
@@ -70,7 +72,7 @@ namespace ModernComputerTechnologies {
         /// Сортировка пузырьком
         /// </summary>
         /// <param name="array">массив</param>
-        static void BubbleSort(int[] array) {
+        internal static void BubbleSort(int[] array) {
             int arrayLength = array.Length;
             for (int i = 0; i < arrayLength - 1; i++) {
                 for (int j = 0; j < arrayLength - i - 1; j++) {
@@ -85,7 +87,7 @@ namespace ModernComputerTechnologies {
         /// Сортировка вставками
         /// </summary>
         /// <param name="array">массив</param>
-        static void InsertSort(int[] array) {
+        internal static void InsertSort(int[] array) {
             int arrayLength = array.Length;
             for (int i = 1; i < arrayLength; i++) {
                 for (int j = i; j > 0 && array[j - 1] > array[j]; j--) {
@@ -98,7 +100,7 @@ namespace ModernComputerTechnologies {
         /// Быстрая сортировка
         /// </summary>
         /// <param name="array">массив</param>
-        static void QuickSort(int[] array) {
+        internal static void QuickSort(int[] array) {
             QuickSort(array, 0, array.Length - 1);
         }
 
@@ -164,6 +166,49 @@ namespace ModernComputerTechnologies {
         }
 
 
+        #endregion
+
+
+        #region Old Implementation
+
+        /// <summary>
+        /// Виды сортировок
+        /// </summary>
+        internal enum SortType {
+            BubbleSort = 0,
+            InsertSort = 1,
+            QuickSort = 2
+        }
+
+        /// <summary>
+        /// Результаты выполнения сортировок
+        /// </summary>
+        /// <param name="array">массив</param>
+        /// <param name="sortType">вид сортировки</param>
+        internal static void SortResult(int[] array, SortType sortType) {
+            Stopwatch stopWatch = new Stopwatch();
+            switch (sortType) {
+                case SortType.BubbleSort:
+                    stopWatch.Start();
+                    BubbleSort(array);
+                    stopWatch.Stop();
+                    break;
+                case SortType.InsertSort:
+                    stopWatch.Start();
+                    InsertSort(array);
+                    stopWatch.Stop();
+                    break;
+                case SortType.QuickSort:
+                    stopWatch.Start();
+                    QuickSort(array);
+                    stopWatch.Stop();
+                    break;
+                default:
+                    return;
+            }
+            Console.WriteLine($"Sorted array <{sortType}>: time = {stopWatch.Elapsed.Minutes}m:{stopWatch.Elapsed.Seconds}s:{stopWatch.Elapsed.Milliseconds}ms\t default time = {stopWatch.Elapsed}");
+            //Console.WriteLine($"Sorted array <{sortType}>: {string.Join(" ,", array.ToArray())}");
+        }
         #endregion
     }
 }
