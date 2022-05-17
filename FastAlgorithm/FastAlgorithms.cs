300348a86fa01234c358ca027bbdb3580813e52b
nearsolt <67660206+nearsolt@users.noreply.github.com>
2022-05-18 00:46:03 +0400

    Закодил  FHWT/FIHWT  в понимании "In Place"

---
+++ FastAlgorithm/FastAlgorithms.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastAlgorithms {
+    class FastAlgorithms {
+        static void Main(string[] args) {
+
+            #region task 1: Fast Haar Wavelet Transform/Fast Inverse Haar Wavelet Transform Main
+
+            //double[] initialArray = new double[] { 10.6, 5.9, 24.1, 6.4, 4.9, 13.5, 10.3, 9.6 };
+            double[] initialArray = new double[] { 7, 3, 4, 10 };
+            //double[] initialArray = new double[] { 6, 12, 15, 15, 14, 12, 120, 116 };
+            int partitionsNumber = initialArray.Length;
+
+            if (partitionsNumber == 1) {
+                Console.WriteLine($"Количество элементов массива {nameof(initialArray)} равно 1, введите корректные данные");
+                Console.ReadKey();
+                return;
+            }
+            if (!Helpers.IsPowerOfTwo(partitionsNumber)) {
+                Console.WriteLine($"Количество элементов массива {nameof(initialArray)} равно {partitionsNumber}, что не является степенью двойки, введите корректные данные");
+                Console.ReadKey();
+                return;
+            }
+            int powerOfTwo = Convert.ToInt32(Math.Log(partitionsNumber, 2));
+            
+            Console.WriteLine($"Входные значения ({nameof(initialArray)}):\n{string.Join(", ", initialArray.ToArray())}.\n");
+
+            double[] FHWTArray = Helpers.InPlaceFHWT((double[])initialArray.Clone(), powerOfTwo);
+            Console.WriteLine($"Значения, полученные после БПХ ({nameof(FHWTArray)}):\n{string.Join(", ", FHWTArray.ToList())}.\n");
+
+            double[] FIHWTArray = Helpers.InPlaceFIHWT((double[])FHWTArray.Clone(), powerOfTwo);
+            Console.WriteLine($"Значения, полученные после обратного БПХ ({nameof(FIHWTArray)}):\n{string.Join(", ", FIHWTArray.ToList())}.\n");
+
+            #endregion
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ FastAlgorithm/Helpers.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastAlgorithms {
+    class Helpers {
+
+        #region task 1: FHWT/FIHWT
+
+        /// <summary>
+        /// Проверка, является ли число степенью двойки (Decrement and Compare)
+        /// </summary>
+        /// <param name="num">вводимое число</param>
+        /// <returns></returns>
+        internal static bool IsPowerOfTwo(int num) {
+            return (num != 0) && ((num & (num - 1)) == 0);
+        }
+
+        /// <summary>
+        /// Быстрое преобразование Хаара (In Place)
+        /// </summary>
+        /// <param name="array">массив значений сигнала</param>
+        /// <param name="powerOfTwo">степень двойки</param>
+        /// <returns></returns>
+        internal static double[] InPlaceFHWT(double[] array, int powerOfTwo) {
+
+            int tmpLength = array.Length;
+            int gap = 2;
+            int increment = 1;
+
+            for (int k = 0; k < powerOfTwo; k++) {
+                tmpLength /= 2;
+                for (int j = 0; j < tmpLength; j++) {
+                    (array[gap * j], array[gap * j + increment]) = ((array[gap * j] + array[gap * j + increment]) / 2.0, (array[gap * j] - array[gap * j + increment]) / 2.0);
+                }
+                increment = gap;
+                gap *= 2;
+            }
+            return array;
+        }
+
+        /// <summary>
+        /// Быстрое обратное преобразование Хаара (In Place)
+        /// </summary>
+        /// <param name="array">массив значений сигнала</param>
+        /// <param name="powerOfTwo">степень двойки</param>
+        /// <returns></returns>
+        internal static double[] InPlaceFIHWT(double[] array, int powerOfTwo) {
+
+            int tmpLength = array.Length;
+
+            int gap = tmpLength;
+            int increment = tmpLength/2;
+            int step = 1;
+
+            for (int k = powerOfTwo; k > 0; k--) {
+                for (int j = 0; j < step; j++) {
+                    (array[gap * j], array[gap * j + increment]) = (array[gap * j] + array[gap * j + increment], array[gap * j] - array[gap * j + increment]);
+                }
+                gap = increment;
+                increment /= 2;
+                step *= 2;
+            }
+            return array;
+        }
+
+
+
+        #endregion
+    }
+}
