60c19fdf525cdd11f2ca6223313c486b0c3bc104
nearsolt <67660206+nearsolt@users.noreply.github.com>
2022-05-26 00:07:31 +0400

    Создание проекта под OpenGL, добавил доп либы:
    FreeGlut из фреймворка Tao - https://sourceforge.net/projects/taoframework/
    
     OpenGL из репозитория - https://github.com/giawa/opengl4csharp/
    
    Создание белого фрейма под первое задание

---
+++ GraficsOpenGL/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tao.FreeGlut;
+using OpenGL;
+
+namespace GraficsOpenGL {
+    class Program {
+
+        private static int width = 800, height = 600;
+
+        private static void OnDisplay() {
+            Gl.ClearColor(1f, 1f, 1f, 1f);
+        }
+
+        private static void OnRenderFrame() {
+            
+            Gl.Viewport(0, 0, width, height);
+            Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Glut.glutSwapBuffers();
+        }
+
+        static void Main(string[] args) {
+            
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Glut.glutInitWindowSize(width, height);
+            Glut.glutCreateWindow("Main Window");
+
+            
+            Glut.glutIdleFunc(OnRenderFrame);
+            Glut.glutDisplayFunc(OnDisplay);
+
+            Glut.glutMainLoop();
+        }
+
+        
+    }
+}
