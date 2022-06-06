3b5b5ff61e8361a12d18286de724bad2c9021db8
nearsolt <67660206+nearsolt@users.noreply.github.com>
2022-06-07 01:31:07 +0400

    #MCT_GUI промежуточный чекин

--- ModernComputerTechnologiesGUI/Helpers.cs
+++ ModernComputerTechnologiesGUI/Helpers.cs
@@ -6,5 +6,33 @@ using System.Threading.Tasks;
 
 namespace ModernComputerTechnologiesGUI {
     class Helpers {
+
+        #region Initial Data
+
+        /// <summary>
+        /// Получение значения функции в точке
+        /// </summary>
+        /// <param name="value">точка</param>
+        /// <returns></returns>
+        internal static double FuncValues(double value) {
+            return value - 2;
+
+                //                Math.Pow(value, 2.0) + value * 0.2 - 3.5;
+        }
+
+
+        internal static Tuple<List<double>, List<double>> Calc() {
+            int n = 5;
+            List<double> x = new List<double>();
+            List<double> f = new List<double>();
+
+            for (int i = 0; i <= n; i++) {
+                x.Add(i);
+                f.Add(Helpers.FuncValues(i));
+            }
+            return new Tuple<List<double>, List<double>>(x,f);
+        }
+
+        #endregion
     }
 }
--- ModernComputerTechnologiesGUI/Program.cs
+++ ModernComputerTechnologiesGUI/Program.cs
@@ -9,11 +9,22 @@ namespace ModernComputerTechnologiesGUI {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
+        //[STAThread]
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //int n = 5;
+            //List<double> x = new List<double>();
+            //List<double> f = new List<double>();
+
+            //for(int i = 0; i <= n; i++) {
+            //    x.Add(i);
+            //    f.Add(Helpers.FuncValues(i));
+            //}
+            //Console.WriteLine($"x: {string.Join(", ", x.ToList())}\n f: {string.Join(", ", f.ToList())}");
+            //Console.ReadLine();
         }
     }
 }
