72ab49c411678d1471a50f8436033d5b1d140662
nearsolt <67660206+nearsolt@users.noreply.github.com>
2022-06-18 14:19:24 +0400

    #WA Создание проекта, реализация алгоритма  БПХ (Ordered)

---
+++ WaveletAnalysis/Helper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaveletAnalysis {
+    class Helper {
+        /// <summary>
+        /// Проверка, является ли число степенью двойки (Decrement and Compare)
+        /// </summary>
+        /// <param name="num">вводимое число</param>
+        /// <returns></returns>
+        internal static bool IsPowerOfTwo(int num) {
+            return (num != 0) && ((num & (num - 1)) == 0);
+        }
+
+        /// <summary>
+        /// Быстрое преобразование Хаара (Ordered)
+        /// </summary>
+        /// <param name="array">массив значений сигнала</param>
+        /// <param name="powerOfTwo">степень двойки</param>
+        /// <returns></returns>
+        internal static double[] OrderedFHWT(double[] array, int powerOfTwo) {
+            int condExpression = array.Length;
+            double[] tmpDiffArray = new double[condExpression / 2];
+
+            for (int k = 0; k < powerOfTwo; k++) {
+                condExpression /= 2;
+                for (int j = 0; j < condExpression; j++) {
+                    tmpDiffArray[j] = (array[2 * j] - array[2 * j + 1]) / 2.0;
+                    array[j] = (array[2 * j] + array[2 * j + 1]) / 2.0;
+                }
+                for (int j = 0; j < condExpression; j++) {
+                    array[j + condExpression] = tmpDiffArray[j];
+                }
+            }
+            return array;
+        }
+
+        /// <summary>
+        /// Быстрое обратное преобразование Хаара (Ordered)
+        /// </summary>
+        /// <param name="array">массив значений сигнала</param>
+        /// <param name="powerOfTwo">степень двойки</param>
+        /// <returns></returns>
+        internal static double[] OrderedFIHWT(double[] array, int powerOfTwo) {
+            int condExpression = 1;
+            double[] tmpDiffArray = new double[array.Length / 2];
+
+            for (int k = 0; k < powerOfTwo; k++) {
+                for (int j = 0; j < condExpression; j++) {
+                    tmpDiffArray[j] = array[j + condExpression];
+                }
+                for (int j = condExpression - 1; j >= 0; j--) {
+                    array[2 * j + 1] = array[j] - tmpDiffArray[j];
+                    array[2 * j] = array[j] + tmpDiffArray[j];
+                }
+                condExpression *= 2;
+            }
+            return array;
+        }
+    }
+}
---
+++ WaveletAnalysis/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaveletAnalysis {
+    class Program {
+        static void Main(string[] args) {
+            double[] initialArray = new double[] { 6, 12, 15, 15, 14, 12, 120, 116 };
+            int numberN = initialArray.Length;
+
+            if (numberN == 1) {
+                Console.WriteLine($"Количество элементов массива {nameof(initialArray)} равно 1, введите корректные данные");
+                Console.ReadKey();
+                return;
+            }
+            if (!Helper.IsPowerOfTwo(numberN)) {
+                Console.WriteLine($"Количество элементов массива {nameof(initialArray)} равно {numberN}, что не является степенью двойки, введите корректные данные");
+                Console.ReadKey();
+                return;
+            }
+            int powerOfTwo = Convert.ToInt32(Math.Log(numberN, 2));
+
+            Console.WriteLine($"Входные значения ({nameof(initialArray)}):\n{string.Join(", ", initialArray.ToArray())}.\n");
+
+            double[] orderedFHWTArray = Helper.OrderedFHWT((double[])initialArray.Clone(), powerOfTwo);
+            Console.WriteLine($"Значения, полученные после БПХ ({nameof(orderedFHWTArray)}):\n{string.Join(", ", orderedFHWTArray.ToArray())}.\n");
+
+            double[] orderedFIHWTArray = Helper.OrderedFIHWT((double[])orderedFHWTArray.Clone(), powerOfTwo);
+            Console.WriteLine($"Значения, полученные после обратного БПХ ({nameof(orderedFIHWTArray)}):\n{string.Join(", ", orderedFIHWTArray.ToArray())}.\n");
+
+            Console.ReadLine();
+        }
+    }
+}
